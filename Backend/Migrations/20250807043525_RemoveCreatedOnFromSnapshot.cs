c8ec6d37110bc939d724855b3e6f04ddbc9abc2c
Lautaro Bertarelli <lautaber9@gmail.com>
2025-08-07 01:41:00 -0300

    Remove CreatedOn for Users

---
+++ Backend/Migrations/20250807043525_RemoveCreatedOnFromSnapshot.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCreatedOnFromSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "79848c11-c652-4b0f-b209-7f578137ede6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c826d480-df7d-439e-a4b0-034fd769ff2c");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "50f75716-438c-495c-98c7-4f68438288b9", null, "user", "USER" },
+                    { "7e2fd730-f437-4fb4-bb10-d1e81a722291", null, "admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "50f75716-438c-495c-98c7-4f68438288b9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7e2fd730-f437-4fb4-bb10-d1e81a722291");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "79848c11-c652-4b0f-b209-7f578137ede6", null, "user", "USER" },
+                    { "c826d480-df7d-439e-a4b0-034fd769ff2c", null, "admin", "ADMIN" }
+                });
+        }
+    }
+}
